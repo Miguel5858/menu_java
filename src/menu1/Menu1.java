6155e6d7d283f14535c4ebc4a8946008dba6b374
ESTUDIANTE <ESTUDIANTE@LABN2MAQ9.ISTJ>
2023-03-18 09:00:29 -0500

    Clase suma

--- src/menu1/Menu1.java
+++ src/menu1/Menu1.java
@@ -4,15 +4,28 @@
  */
 package menu1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author I N T R I A G O 
  */
 public class Menu1 {
+public static void sumar(){
+     int num1,num2,resultado;
+     JOptionPane.showMessageDialog(null,"Ud ingreso a la opcion sumar: ");
+    num1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese numero 1"));
+    num2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese numero 2"));
+    resultado = num1 + num2;
+    JOptionPane.showMessageDialog(null,"El resultado es: "+ resultado);
+    }
+
 
  
     public static void main(String[] args) {
-        // TODO code application logic here
+
+      
+        
     }
     
 }
