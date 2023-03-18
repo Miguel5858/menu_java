4c76bac03618cd12ad0160362362e05acb3acc58
ESTUDIANTE <ESTUDIANTE@LABN2MAQ9.ISTJ>
2023-03-18 08:39:53 -0500

    Premera subda

---
+++ src/menu1/Menu1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package menu1;
+
+/**
+ *
+ * @author I N T R I A G O 
+ */
+public class Menu1 {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
